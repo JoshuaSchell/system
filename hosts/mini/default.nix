9401568cf796c86843c5af081775d85e325676a5
Josh Schell <josh@schell.me>
2025-11-22 23:43:44 -0700

    added default for mini

---
+++ hosts/mini/default.nix
@@ -0,0 +1,27 @@
+lib: lib.buildDarwinSystem (
+  { lib, pkgs, ... }:
+  let
+    inherit (lib) collectNix remove;
+  in {
+    imports = collectNix ./. |> remove ./default.nix;
+
+    type = "server";
+
+    nixpkgs.config.allowUnfree = true;
+
+    networking.hostName = "josh";
+
+    users.users.josh = {
+      name = "josh";
+      home = "/Users/josh";
+      shell = pkgs.nushell;
+    };
+
+    home-manager.users.josh.home = {
+      stateVersion = "25.05";
+      homeDirectory = "/Users/josh";
+    };
+
+    system.stateVersion = 5;
+  }
+)
